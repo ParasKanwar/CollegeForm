84630171571efe32127e7bb69d5aacc0ecdca6a1
paras <paraskanwar30@gmail.com>
2020-12-29 15:41:15 +0530

    removed

--- components/login/baseContainer.tsx
+++ components/login/baseContainer.tsx
@@ -1,7 +1,16 @@
-import { PropsWithChildren } from "react";
-
+import { PropsWithChildren, useContext } from "react";
+import theme from "../../context/theme/theme";
+import styles from "./baseContainer.module.css";
 export default (props: PropsWithChildren<{}>) => {
+  const context = useContext(theme);
   return (
-    <div style={{ width: "300px", height: "300px" }}>{props.children}</div>
+    <div
+      style={{
+        background: context.dark_mode ? "#121212" : "rgba(255,255,255,1)",
+      }}
+      className={styles.baseContainer}
+    >
+      {props.children}
+    </div>
   );
 };
--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,39 +1,18 @@
 import dynamic from "next/dynamic";
+import { motion } from "framer-motion";
 import React, { useContext } from "react";
 import Theme from "../../context/theme/theme";
-const Frame = dynamic(async () => (await import("framer")).Frame);
+import BaseContainer from "./baseContainer";
 export default () => {
   const context = useContext(Theme);
   return (
-    <Frame
-      color={"black"}
-      initial={{ scale: 0.1 }}
-      animate={{
-        scale: 1,
-        animationDelay: "1s",
-        background: context.dark_mode ? "#ffffff" : "#212121",
-      }}
-      borderRadius="10px"
-      width="330px"
-      height="400px"
-      shadow="0px 0px 10px 10px rgba(0,0,0,0.1)"
-      dragConstraints={{ bottom: 0, left: 0, right: 0, top: 0 }}
-      dragMomentum={true}
-      drag={true}
-      transition={{ type: "spring", duration: 0.5 }}
-      center
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div style={{ display: "flex" }}>
-          <img src="/icon-512x512.png" width="60px" height="60px"></img>
-        </div>
-      </div>
-    </Frame>
+    <motion.div drag dragConstraints={{ bottom: 0, left: 0, right: 0, top: 0 }}>
+      <BaseContainer>
+        <input></input>
+        <input></input>
+        <button>Sign_In</button>
+        <button>Sign_Up</button>
+      </BaseContainer>
+    </motion.div>
   );
 };
